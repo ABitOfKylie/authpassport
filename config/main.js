0920a371b2d49de2e2ea1b4636f0be96b20455d6
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2016-11-27 18:11:10 -0800

    First Commit. In early stage, simple app.get, res.send worked with localhost:3000. This version includes user.model, api routes, passport/jwt authentication, bcrypt hash and mongoose.connect. When trying to connect to localhost:27017 getting following error: AssertionException handling request, closing client connection: 17132 SSL handshake received but server is started without SSL support

---
+++ config/main.js
@@ -0,0 +1,8 @@
+module.exports = {  
+'secret': 'putsomethingtopsecrethere',  
+'database': 'mongodb://127.0.0.1:27017/authpassport'  
+};
+
+// module.exports = {  
+// 'port': process.env.PORT || 3000  
+// }
---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');  
+var morgan = require('morgan');  
+var passport = require('passport'); 
+
+// db id/name and secret stored in main
+var config = require('./config/main'); 
+var User = require('./app/models/user');
+var jwt = require('jsonwebtoken');  
+
+app = express();
+
+// use body-parser to get POST requests for API use
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());
+
+// Log requests to console
+app.use(morgan('dev')); 
+
+app.use (passport.initialize()); 
+
+// connect to db
+mongoose.connect(config.database);
+
+// Bring in passport strategy
+require('./config/passport')(passport);
+
+// API routes
+ apiRoutes.post ('./register', function(req,res){
+ 	if(!req.body.email || !req.body.password){
+ 		res.json({success:false, message: "Please enter an email and password to register."});
+	} else {
+		var newUser = newUser({
+			email: req.body.email;
+			password: req.body.password;
+		});
+	},
+
+// attempt to save new user / .save is mongoose method
+	newUser.save (function(err){
+		if (err) {
+ 		res.json({success:false, message: "That email already exists."});
+		}
+ 		res.json({success:true, message: "Successfully created new user!"});
+ 	});
+
+});
+
+// Authenticate the user and get a JSON Web Token to include in the header of future requests.
+apiRoutes.post('/authenticate', function(req, res) {  
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({ success: false, message: 'Authentication failed. User not found.' });
+    } else {
+      // Check if password matches
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          // Create token if the password matched and no error was thrown
+          var token = jwt.sign(user, config.secret, {
+            expiresIn: 10080 // in seconds
+          });
+          res.json({ success: true, token: 'JWT ' + token });
+        } else {
+          res.send({ success: false, message: 'Authentication failed. Passwords did not match.' });
+        }
+      });
+    }
+  });
+});
+
+// Protect dashboard route with JWT
+apiRoutes.get('/dashboard', passport.authenticate('jwt', { session: false }), function(req, res) {  
+  res.send('It worked! User id is: ' + req.user._id + '.');
+});
+
+// Set url for API group routes
+app.use('/api', apiRoutes);  
+// Home route. We'll end up changing this to our main front end index later.
+app.get('/', function(req, res) {  
+  res.send('Relax. We will put the home page here later.');
+});
+
+var port = 3000;
+app.listen(port);
+console.log("Ta dah! Your server is now running on port " + port + ".");
